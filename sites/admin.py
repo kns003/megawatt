67ca6626dea448e473e87a7d719cec8f94bcdaa4
shashank <kns003@gmail.com>
2018-08-30 18:49:27 +0530

    changes

--- sites/admin.py
+++ sites/admin.py
@@ -7,7 +7,7 @@ class SiteAdmin(admin.ModelAdmin):
     list_display = ('get_site_name', 'a_value', 'b_value')
 
     def get_site_name(self, obj):
-        return obj.name
+        return obj.site_name.name
     get_site_name.short_description = 'Site Name'
 
 @admin.register(SiteName)
