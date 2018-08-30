01a7ec86986320501b79c09920c2c4d01aa98f38
shashank <kns003@gmail.com>
2018-08-30 18:46:06 +0530

    adding list display

--- sites/admin.py
+++ sites/admin.py
@@ -4,7 +4,11 @@ from .models import Sites, SiteName
 # Register your models here.
 @admin.register(Sites)
 class SiteAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('get_site_name', 'a_value', 'b_value')
+
+    def get_site_name(self, obj):
+        return obj.name
+    get_site_name.short_description = 'Site Name'
 
 @admin.register(SiteName)
 class SiteNameAdmin(admin.ModelAdmin):
